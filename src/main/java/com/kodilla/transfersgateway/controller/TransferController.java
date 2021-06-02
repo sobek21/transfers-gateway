05b1e4fa650da0f643002cc85e65c38d774b91f5
sobek21 <mateuszsobkowicz211@gmail.com>
2021-06-02 19:43:10 +0200

    Initial commit

---
+++ src/main/java/com/kodilla/transfersgateway/controller/TransferController.java
@@ -0,0 +1,38 @@
+package com.kodilla.transfersgateway.controller;
+
+import com.kodilla.commons.Transfer;
+import com.kodilla.transfersgateway.controller.request.TransferRequest;
+import com.kodilla.transfersgateway.service.TransferProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("v1/transfers")
+@RequiredArgsConstructor
+public class TransferController {
+
+  private final TransferProducer transferProducer;
+
+  @PostMapping
+  public void saveTransfer(@RequestBody TransferRequest request) {
+    log.info("Received transfer request: {}", request);
+    Transfer transfer = new Transfer();
+    transfer.setAmount(request.getAmount());
+    transfer.setRecipientAccount(request.getRecipientAccount());
+    transfer.setSenderAccount(request.getSenderAccount());
+    transfer.setTitle(request.getTitle());
+
+    transferProducer.sendTransfer(transfer);
+
+  }
+  @GetMapping("/a")
+  public String test1() {
+    return "tal";
+  }
+}
